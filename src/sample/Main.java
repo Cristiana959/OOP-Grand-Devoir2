7803ab08ca1f99ce1b073e1723a4a733fdb969f9
Sebastian2000 <naesebi2000@gmail.com>
2020-12-22 23:17:11 +0200

    daca as stii ce face aici e bine

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,109 +1,334 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class Main extends Application {
 
-    int counter = 0;
-    int nr_check = 0;
-    int num_Questions = 1;
+    public int counter = 1;
+    public int nr_check = 0;
+    public int num_Questions = 1;
+    public int row = 0;
+    public int colum = 0;
 
-    public static void addItemsToGrid(GridPane g, ArrayList<Node> arrayList)
-    {
-        int i = 0,j=0,k=0,t=0;
-        for(Node obj : arrayList)
-        {
-            g.add(obj,i,j++,k++,t++);
-        }
+
+    public ArrayList<Question> questionArrayList = new ArrayList<>();
+    public ArrayList<String> answers = new ArrayList<>();
+    public ArrayList<String> good_answers = new ArrayList<>();
+
+    public ArrayList<TextField> responseTextFieldArray = new ArrayList<>();
+    public ArrayList<CheckBox> responseCheckBoxArray = new ArrayList<>();
+    public ArrayList<RadioButton> responseRadio = new ArrayList<>();
+
+
+    Label question_label = new Label("Question no " + num_Questions);
+
+    Button prev_question = new Button("Prev");
+    Button next_question = new Button("Next");
+
+    Label type_question = new Label("Type the question");
+
+    TextField question_text = new TextField();
+
+    Label type_answer = new Label("Type the answer");
+
+    Label choices = new Label("No of choices");
+
+    Button minus_question = new Button("-");
+    Button plus_question = new Button("+");
+
+    RadioButton single_answer = new RadioButton("Unique answer");
+    RadioButton multiple_answer = new RadioButton("Multiple answers");
+
+    TextField nr_choice = new TextField(String.valueOf(counter));
+
+    Button save = new Button("Save question");
+    Button gen_html = new Button("Generate HTML");
+    Button clear_reponses = new Button("Clear reponse");
+
+    Button open_test = new Button("Open Test");
+    Button save_test = new Button("Save Test");
+    Button exit = new Button("Exit");
+    Button test = new Button("TEST");
+
+    public GridPane Top_UI(GridPane gp) {
+        gp.add(question_label, 0, 0);
+        gp.add(prev_question, 0, 1);
+        gp.add(next_question, 1, 1);
+
+        return gp;
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello World");
+    public GridPane Left_UI(GridPane gp) {
+        gp.add(choices, 0, 0);
+        gp.add(minus_question, 0, 1);
+        gp.add(nr_choice, 1, 1);
+        gp.add(plus_question, 2, 1);
 
+        gp.add(single_answer, 0, 2);
+        gp.add(multiple_answer, 0, 3);
 
-        GridPane gridPane = new GridPane();
-        //gridPane.add(question,0,0,2,1);
+        VBox vbox = new VBox();
+        vbox.getChildren().add(save);
+        vbox.getChildren().add(gen_html);
+        vbox.getChildren().add(clear_reponses);
 
+        gp.add(vbox,0,5);
 
-        TextField question = new TextField();
+        return gp;
+    }
 
-        Label test = new Label();
-
-        Button next_button = new Button();
-        Button prev_button = new Button();
-
-        RadioButton one_answer =new RadioButton();
-        RadioButton multiple_answer = new RadioButton();
-
-        ArrayList<Node> arrayList = new ArrayList<>();
-        arrayList.add(next_button);
-        arrayList.add(prev_button);
-
-        /** Button for next question*/
-        next_button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                //test.setText(String.valueOf(counter++));
-            }
-        });
-
-        /** Button for prev question*/
-        prev_button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                //test.setText(String.valueOf(counter--));
-            }
-        });
-
-        /***/
-        one_answer.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                gridPane.add(new CheckBox("one answer"),2,0);
-            }
-        });
+    public GridPane Right_UI(GridPane gp) {
+        GridPane m_gird = new GridPane();
+        gp.add(type_question, 0, 0);
+        gp.add(question_text, 0, 1);
 
         multiple_answer.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                gridPane.add(new CheckBox("1"),2,0);
-                gridPane.add(new CheckBox("2"),3,0);
-                gridPane.add(new CheckBox("3"),4,0);
+                dinamicGridMulti(m_gird).getChildren().clear();
+                dinamicGridSingle(m_gird).getChildren().clear();
+                gp.add(dinamicGridMulti(m_gird), 0, 4);
             }
         });
 
-
-        question.setOnAction(new EventHandler<ActionEvent>() {
+        single_answer.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                //test.setText(question.getText());
+                dinamicGridSingle(m_gird).getChildren().clear();
+                dinamicGridMulti(m_gird).getChildren().clear();
+                gp.add(dinamicGridSingle(m_gird),0,4);
+
             }
         });
 
+        clear_reponses.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                dinamicGridSingle(m_gird).getChildren().clear();
+                dinamicGridMulti(m_gird).getChildren().clear();
+                single_answer.setSelected(false);
+                multiple_answer.setSelected(false);
+
+            }
+        });
+
+        nr_choice.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                if(single_answer.isSelected())
+                {
+                    dinamicGridSingle(m_gird).getChildren().clear();
+                    dinamicGridMulti(m_gird).getChildren().clear();
+                    gp.add(dinamicGridSingle(m_gird),0,4);
+                }
+                else if(multiple_answer.isSelected())
+                {
+                    dinamicGridMulti(m_gird).getChildren().clear();
+                    dinamicGridSingle(m_gird).getChildren().clear();
+                    gp.add(dinamicGridMulti(m_gird), 0, 4);
+                }
+            }
+        });
+
+        return gp;
+    }
+
+    public GridPane dinamicGridMulti(GridPane gridPane) {
+        int aux_rep = Integer.parseInt(nr_choice.getText());
+        if (aux_rep == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.show();
+        } else {
+            for (int i = 0; i < aux_rep; i++, row++) {
+                colum = 0;
+                CheckBox checkBox = new CheckBox(String.valueOf(i));
+                TextField response1 = new TextField();
+                gridPane.add(checkBox, colum, row);
+                colum = colum + 1;
+                gridPane.add(response1, colum, row);
+                responseTextFieldArray.add(response1);
+                responseCheckBoxArray.add(checkBox);
+            }
+        }
+        return gridPane;
+    }
+
+    public GridPane dinamicGridSingle(GridPane gridPane)
+    {
+        final ToggleGroup check_reponse = new ToggleGroup();
+        int aux_rep = Integer.parseInt(nr_choice.getText());
+        if (aux_rep == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.show();
+        } else {
+            for (int i = 0; i < aux_rep; i++, row++) {
+                colum = 0;
+                RadioButton radioReponse = new RadioButton(String.valueOf(i));
+                TextField response1 = new TextField();
+                gridPane.add(radioReponse, colum, row);
+                colum = colum + 1;
+                gridPane.add(response1, colum, row);
+                radioReponse.setToggleGroup(check_reponse);
+                responseTextFieldArray.add(response1);
+                responseRadio.add(radioReponse);
+            }
+        }
+        return gridPane;
+    }
+
+    public HBox Button_UI(HBox hbox) {
+        hbox.getChildren().add(open_test);
+        hbox.getChildren().add(save_test);
+        hbox.getChildren().add(exit);
+
+        return hbox;
+    }
+
+    public void transformArrayTextBox()
+    {
+        for(TextField tf : responseTextFieldArray)
+        {
+            answers.add(tf.getText());
+        }
+    }
+
+    public void transformArrayCheckBox()
+    {
+        for(int i=0;i<responseRadio.size();i++)
+        {
+            if(responseRadio.get(i).isSelected())
+            {
+                good_answers.add(responseTextFieldArray.get(i).getText());
+            }
+        }
+    }
+
+    public void transformArrayRadio()
+    {
+        for(int i=0;i<responseCheckBoxArray.size();i++)
+        {
+            if(responseCheckBoxArray.get(i).isSelected())
+            {
+                good_answers.add(responseTextFieldArray.get(i).getText());
+            }
+        }
+    }
+
+    public void UI(Stage scene) {
+
+        FileChooser fileChooser = new FileChooser();
+
+        ToggleGroup group = new ToggleGroup();
+        single_answer.setToggleGroup(group);
+        multiple_answer.setToggleGroup(group);
+
+        next_question.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                num_Questions++;
+                question_text.clear();
+                nr_choice.clear();
+            }
+        });
+
+        plus_question.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                counter = counter + 1;
+                nr_choice.setText(String.valueOf(counter));
+            }
+        });
+
+        minus_question.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (counter <= 0) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.show();
+                } else {
+                    counter = counter - 1;
+                    nr_choice.setText(String.valueOf(counter));
+                }
+            }
+        });
+
+        GridPane gridPane = new GridPane();
+        GridPane gridPane1 = new GridPane();
+        GridPane gridPane2 = new GridPane();
+        GridPane gridPane3 = new GridPane();
+        BorderPane borderPane = new BorderPane();
+
+        borderPane.setBottom(Button_UI(new HBox()));
+        borderPane.setTop(Top_UI(gridPane1));
+        borderPane.setRight(Left_UI(gridPane2));
+        borderPane.setLeft(Right_UI(gridPane3));
 
 
-        gridPane.add(test,0,1,1,1);
-        //gridPane.add(next_button,1,0);
-        //gridPane.add(prev_button,0,0);
-        gridPane.add(one_answer,1,0);
-        gridPane.add(multiple_answer,0,0);
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.exit(0);
+            }
+        });
+
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
+            if (keyEvent.equals(KeyCode.ENTER)) {
+
+            } else if (keyEvent.getCode().equals(KeyCode.ESCAPE)) {
+                System.exit(0);
+            }
+        });
+
+        transformArrayCheckBox();
+        transformArrayTextBox();
+        transformArrayRadio();
+
+        save.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Question question = new Question(question_text.getText(),good_answers.size(),answers,good_answers);
+                System.out.println(question.toString());
+            }
+        });
+
+        prev_question.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                gridPane.getChildren().clear();
+            }
+        });
+
+        scene.setScene(new Scene(borderPane, 1280, 720));
+        scene.show();
+    }
 
 
-        primaryStage.setScene(new Scene(gridPane,500,800));
-        primaryStage.show();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        UI(primaryStage);
     }
 
 
--- src/sample/Question.java
+++ src/sample/Question.java
@@ -5,6 +5,55 @@ import java.util.ArrayList;
 public class Question {
     private String name;
     private int numberOfAnswers;
-    ArrayList<String> answers = new ArrayList<>();
+    private ArrayList<String> responses = new ArrayList<>();
+    private ArrayList<String> answers = new ArrayList<>();
 
+    public Question(String name, int numberOfAnswers, ArrayList<String> responses, ArrayList<String> answers) {
+        this.name = name;
+        this.numberOfAnswers = numberOfAnswers;
+        this.responses = responses;
+        this.answers = answers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfAnswers() {
+        return numberOfAnswers;
+    }
+
+    public void setNumberOfAnswers(int numberOfAnswers) {
+        this.numberOfAnswers = numberOfAnswers;
+    }
+
+    public ArrayList<String> getResponses() {
+        return responses;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "name='" + name + '\'' +
+                ", numberOfAnswers=" + numberOfAnswers +
+                ", responses=" + responses +
+                ", answers=" + answers +
+                '}';
+    }
+
+    public void setResponses(ArrayList<String> responses) {
+        this.responses = responses;
+    }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(ArrayList<String> answers) {
+        this.answers = answers;
+    }
 }
